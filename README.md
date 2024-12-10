71f0d479a5214509fe711511bbd6d9dbd350ac4e
siddheshwarshingare <144676414+siddheshwarshingare@users.noreply.github.com>
2024-12-10 18:12:51 +0530

    tt

---
+++ README.md
@@ -0,0 +1,1 @@
+<img src="https://github.com/siddheshwarshingare/account_management_application/blob/master/outputs/result.gif" height="400">
---
+++ android/app/src/main/kotlin/com/example/account_management_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.account_management_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
